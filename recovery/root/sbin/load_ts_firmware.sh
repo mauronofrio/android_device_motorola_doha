d5475c9c674cfce1c522d03b050d2f0d51ec961d
Vachounet <vachounet@live.fr>
2020-03-08 19:10:27 +0100

    doha: load modules before firmwares
    Fix touchscreen

--- recovery/root/sbin/load_ts_firmware.sh
+++ recovery/root/sbin/load_ts_firmware.sh
@@ -5,15 +5,6 @@ touch_path=
 firmware_path=/vendor/firmware
 module_path=/sbin/modules
 
-cd $firmware_path
-
-for touch_product_string in $(ls $touch_class_path); do
-    touch_path=/sys$(cat $touch_class_path/$touch_product_string/path)
-    firmware_file=$(ls *$touch_product_string*)
-    echo 1 > $touch_path/forcereflash
-    echo $firmware_file > $touch_path/doreflash
-done
-
 # Load all needed modules
 insmod $module_path/aw8695.ko
 insmod $module_path/focaltech_0flash_mmi.ko
@@ -29,5 +20,14 @@ insmod $module_path/stmvl53l0.ko
 insmod $module_path/sx933x_sar.ko
 insmod $module_path/tps61280.ko
 insmod $module_path/watchdog_cpu_ctx.ko
+
+cd $firmware_path
+
+for touch_product_string in $(ls $touch_class_path); do
+    touch_path=/sys$(cat $touch_class_path/$touch_product_string/path)
+    firmware_file=$(ls *$touch_product_string*)
+    echo 1 > $touch_path/forcereflash
+    echo $firmware_file > $touch_path/doreflash
+done
 
 return 0
